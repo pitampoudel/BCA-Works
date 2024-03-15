ce1a32457c3560c135452dba9abd43f0bd74a189
Pitam <pitampoudelsaipu@gmail.com>
2024-03-15 07:15:59 +0545

    reformat

--- DSA/queue.cpp
+++ DSA/queue.cpp
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define ARR_SIZE 5
 
@@ -7,71 +7,87 @@ int arr[ARR_SIZE];
 int front = -1;
 int rear = -1;
 
-void reset(){
+void reset()
+{
 	rear = -1;
 	front = -1;
 }
 
-void enqueue(){
-	if(rear!=ARR_SIZE-1){
+void enqueue()
+{
+	if (rear != ARR_SIZE - 1)
+	{
 		printf("Please enter a number to enqueue into queue: ");
 		int n;
-		scanf("%d",&n);
-		if(front == -1) front = 0;
+		scanf("%d", &n);
+		if (front == -1)
+			front = 0;
 		rear++;
 		arr[rear] = n;
-		printf("%d came into the queue\n\n",arr[rear]);
-	
-	}else{
+		printf("%d came into the queue\n\n", arr[rear]);
+	}
+	else
+	{
 		printf("Array is full\n");
 	}
 }
 
-void dequeue(){
-	if(front!= -1){
-	printf("%d going out from the queue\n\n",arr[front]);
-	if(front == rear -1) reset(); else front++;
-	}else{
+void dequeue()
+{
+	if (front != -1)
+	{
+		printf("%d going out from the queue\n\n", arr[front]);
+		if (front == rear - 1)
+			reset();
+		else
+			front++;
+	}
+	else
+	{
 		printf("Queue is empty\n");
 	}
-	
 }
 
-
-void display(){
-	if(rear!=-1){
+void display()
+{
+	if (rear != -1)
+	{
 		printf("Queue is (rear to front):\n");
-		for(int i=rear; i>=front; i--){
-			printf("%d\n",arr[i]);
+		for (int i = rear; i >= front; i--)
+		{
+			printf("%d\n", arr[i]);
 		}
 		printf("\n");
-	}else{
+	}
+	else
+	{
 		printf("Queue is empty\n");
 	}
-	
 }
 
-int main(){
+int main()
+{
 	int choice;
-	while(choice !=4){
-	printf("Please choose an option below:\n");
-	printf("1.Enqueue\n2.Dequeue\n3.Display\n4.Stop\n");
+	while (choice != 4)
+	{
+		printf("Please choose an option below:\n");
+		printf("1.Enqueue\n2.Dequeue\n3.Display\n4.Stop\n");
 
-	scanf("%d",&choice);
-	
-	switch(choice){
+		scanf("%d", &choice);
+
+		switch (choice)
+		{
 		case 1:
 			enqueue();
 			break;
 		case 2:
-		   dequeue();
-	 	   break;
+			dequeue();
+			break;
 		case 3:
-	     display();
-	      break;	
-			
-	 }
-   }
+			display();
+			break;
+		}
+	}
 
 	return 0;
 }

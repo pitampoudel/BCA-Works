76644e030ebfde95ceac2fdf12ff673a6ac315f2
Pitam <pitampoudelsaipu@gmail.com>
2024-06-15 22:54:49 +0545

    jdbc

--- Java/DemoProject/build.gradle.kts
+++ Java/DemoProject/build.gradle.kts
@@ -10,8 +10,10 @@ repositories {
 }
 
 dependencies {
+    implementation("com.oracle.database.jdbc:ojdbc8-production:19.7.0.0")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.mysql:mysql-connector-j:8.4.0")
 }
 
 tasks.test {
--- Java/DemoProject/src/main/java/pitam/jdbc/JdbcExample.java
+++ Java/DemoProject/src/main/java/pitam/jdbc/JdbcExample.java
@@ -1,2 +1,30 @@
-package pitam.jdbc;public class JdbcExample {
+package pitam.jdbc;
+
+import java.sql.*;
+import java.sql.SQLException;
+
+public class JdbcExample {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        String url = "localhost";
+        String username = "root";
+        String password = "";
+        String query = "insert into users values( 'test user')";
+
+        // Loading and registering drivers
+        Class.forName("com.mysql.jdbc.Driver");
+
+        // Obtain a connection
+        Connection con = DriverManager.getConnection(url, username, password);
+
+        // Obtain a statement
+        Statement st = con.createStatement();
+
+        // Execute the query
+        int count = st.executeUpdate(query);
+        System.out.println("number of rows affected by this query= " + count);
+
+        // Closing the connection as per the
+        // requirement with connection is completed
+        con.close();
+    }
 }

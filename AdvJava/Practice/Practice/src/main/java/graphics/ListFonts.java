69483e8facbed9da4753ec0b97860cf9b9c71946
Pitam <pitampoudelsaipu@gmail.com>
2025-11-07 07:50:03 +0545

    enhance font listing utility with a Swing-based GUI featuring scrollable font display

--- AdvJava/Practice/Practice/src/main/java/graphics/ListFonts.java
+++ AdvJava/Practice/Practice/src/main/java/graphics/ListFonts.java
@@ -1,12 +1,35 @@
 package graphics;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class ListFonts {
+
     public static void main(String[] args) {
         String[] fontNames = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+        JFrame frame = new JFrame("List Fonts");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setLocationRelativeTo(null);
+
+        JPanel mainPanel = new JPanel(new BorderLayout());
+
+        //fonts
+        JPanel fontPanel = new JPanel();
+        fontPanel.setLayout(new BoxLayout(fontPanel, BoxLayout.Y_AXIS));
         for (String fontName : fontNames) {
-            System.out.println(fontName);
+            JLabel text1 = new JLabel(fontName);
+            text1.setFont(new Font(fontName, Font.PLAIN, 20));
+            text1.setAlignmentX(Component.CENTER_ALIGNMENT);
+            fontPanel.add(text1);
         }
+
+        // scroll pane
+        JScrollPane scrollPane = new JScrollPane(fontPanel);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+        mainPanel.add(scrollPane, BorderLayout.CENTER);
+        frame.add(mainPanel);
+        frame.setVisible(true);
     }
 }
